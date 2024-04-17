bd686fbc8d908fd79deefa9429391d45c74dfca2
Luka Dekanozishvili <luka.dekanozishvili1@gmail.com>
2024-04-17 03:49:45 +0200

    Added luka.c

---
+++ luka.c
@@ -0,0 +1,8 @@
+#define          main       main      (int       argc,       char**b
+#define          G( x       )((x-     ( -6      ))          )/*  */
+typedef          char       q  ;;     /* *    main          /     */               
+#include         /*+*       C/*/      <stdio.h>            /*      */
+int main         ) { fprintf   (      stdout  /*          *///loveyou/  
+,"%c",G(          G((int /**/ )       (3,'@'  ))))        ;;;        //
+static  q /*+*/   c= /*c*/4  ;        printf   ("U"     "%c%c"        ,
+G(G(1104>>-2)),     G(';')) ;         return   -4+c;}   extern        G;
