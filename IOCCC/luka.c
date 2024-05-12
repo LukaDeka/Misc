7c25323ddfd643b8cf5a53452d066165cae25b78
Luka Dekanozishvili <luka.dekanozishvili1@gmail.com>
2024-05-12 15:24:43 +0200

    initial commit

---
+++ IOCCC/luka.c
@@ -0,0 +1,12 @@
+#define          main       main      (int       argc,       char**b
+#define          G( x       )((x-     ( -6      ))          )/*  */
+typedef          char       q; ;;     /* *    main          /     */
+#include         /*+*       C/*/      <stdio.h>            /*      */
+int main         ) {;fprintf   (      stdout  /*          *///typedef/
+,"%c",G(          G((int /**/ )       (3,'@'  ))))        ;;;        //
+static  q /*+*/   c= /*c*/4;;;        printf   ("U"     "%c%c"        ,
+G(G(1104>>-2)),     G(';'));;         return   -4+c;}   extern        G;
+
+
+
+/*             Program outputs "LUKA" after execution                 */
